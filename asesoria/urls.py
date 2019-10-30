150bd6570f57fe47703aeb119d98ed4b0e19ce3e
jbkx2 <braykal@hotmail.com>
2019-10-30 16:43:11 -0300

    Solucione el problema de novedades

--- asesoria/urls.py
+++ asesoria/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
 	path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('horarios/', include('apps.horarios.urls')),
     path('', include('apps.novedades.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
