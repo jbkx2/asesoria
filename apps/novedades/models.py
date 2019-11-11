dfd9dcc3efb851b14db84b99b1cc12a002a22f0f
jbkx2 <braykal@hotmail.com>
2019-11-11 14:40:37 -0300

    Modifique

--- apps/novedades/models.py
+++ apps/novedades/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 class Novedad(models.Model):
 
-	titulo = models.CharField(max_length=50, help_text='Obligatorio')
-	texto = models.TextField(null=True, blank=True)
+	titulo = models.CharField(max_length=60, help_text='Obligatorio')
+	fecha_pub = models.DateField('Ingrese Fecha de Publicacion')
+	desc = models.CharField(max_length=200)
+	contenido = models.TextField(max_length=600)
 	imagen = models.ImageField(upload_to='asesoria/novedades' ,help_text='Opcional', blank=True, null=True)
 
 	def __str__(self):
-		return '%s' % (self.titulo)
+		return '%s, %s' % (self.titulo, self.fecha_pub)
+
+	class Meta():
+		ordering = ['fecha_pub']
+		verbose_name_plural = 'noticias'
